3b72371f5c4f52f2abf34910b1e515f51354d75e
DESKTOP-2JI1BTP\solomon02 <gold0073@naver.com>
2021-04-02 16:45:47 +0900

    commit first msDocker

---
+++ postgreboard/app.js
@@ -0,0 +1,17 @@
+//var morgan = require('morgan');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+//app.use(morgan('dev'));
+
+app.use(require('./postgreRouter'));
+app.use(handleError);
+
+app.listen(9050);
+
+function handleError(err , req, res, next){
+    res.status(err.code).send({msg:err.message});
+}
