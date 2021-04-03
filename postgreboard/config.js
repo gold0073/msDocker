989bb94f1c4507aea3a09363a1e24f418ccaa5ec
jyPark <gold0073@naver.com>
2021-04-03 19:42:15 +0900

    commit local docker

--- postgreboard/config.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg");
-module.exports.postgre_pool =new Pool({
-        host: 'localhost',
-        user: 'postgres',
-        password: 'park0070!',
-        database: 'monolithic',
-        port : '5432'
-    });
