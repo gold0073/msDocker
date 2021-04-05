252c9e29bd35bf71bc2cafa4a4bfee8577fe1e18
DESKTOP-2JI1BTP\solomon02 <gold0073@naver.com>
2021-04-05 16:16:30 +0900

    commit docker-compose Rest-Api

--- postgreboard/src/app.js
+++ postgreboard/src/app.js
@@ -4,15 +4,21 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var app = express();
 
+//const options = {
+//	host: "0.0.0.0",
+//    port: 9150
+//}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 //app.use(morgan('dev'));
 
 //app.use(require('./postgreRouter'));
 app.use(require('./sample'));
+app.use(require('./postgreRouter'));
 app.use(handleError);
 
-app.listen(9050);
+app.listen(9150);
 
 function handleError(err , req, res, next){
     res.status(err.code).send({msg:err.message});
--- postgreboard/src/postgreRouter.js
+++ postgreboard/src/postgreRouter.js
@@ -10,27 +10,28 @@ const { Client } = require("pg");
 const Query = require('pg').Query;
 
 //Search list
-router.get('/ms_contentlist', contentlist);
+router.get('/api/ms-contentlist', contentlist);
 
-/*
-//Search Id
-router.get('/ms_contentlist_id', contentlist_id);
 
-//add Movie
-router.post('/ms_addContent' , addContent);
+//Search detail
+router.get('/api/ms-contentlist/:id', contentlist_detail);
 
-//update Movie
-router.post('/ms_updateContent' , updateContent);
 
-//DeleteMovie
-router.delete('/deleteContent',deleteContent);
-*/
+//add Content
+router.post('/api/ms-addContent' , addContent);
+
+
+//update Content
+router.post('/api/ms-updateContent' , updateContent);
+
+
+//Delete Content
+router.post('/api/ms-deleteContent',deleteContent);
+
 
 
 //Find Type1
 function contentlist(req,res,next){ 
-    var response = {
-    };
 
     //전체리스트
     const pg_query = 
@@ -40,120 +41,148 @@ function contentlist(req,res,next){
     `;
     console.log("Query ==>",pg_query);
 
-    const query = new Query(pg_query);
+    const sendquery = new Query(pg_query);
+    pool.query(sendquery);
+    var rows = []; 
 
-    pool.query(query,(error,cb)=> {
-        if (error || results.rowCount == 0) {
-            response.errorcode = 1;
-            response.errormessage = error ? error : "no data";
-            console.log("Data ===>","No data");
-        } else {
-            response.results = results.rows;
-        }
-        cb(response);
-    });
-
-}
-
-/*
-function contentlist_id(req,res,next){ 
+    sendquery.on("row",row=>{ rows.push(row); });
     
-    pg.connect(con, function(err, client) 
-    {
-        var param_contentid = req.body["content_id"] || req.body.content_id || params.content_id;
-       
-        var pg_query = 
-            ` SELECT * FROM CONTENT CT 
-            INNER JOIN USERS U on U.user_id = CT.user_id 
-            WHERE CT.content_id = $1`;
+    sendquery.on('end', () => { 
+        console.log(rows); 
+        console.log('query done'); 
+        res.send(rows); res.status(200).end(); 
+    }); 
 
-        var query = client.query(pg_query,[param_contentid]); 
-        var rows = []; 
-        query.on('row', function(row){ 
-            rows.push(row); 
-        }); 
-        
-        query.on('end', function(row,err){ 
-            response.render('index', { 
-                title: 'Express', data:rows 
-            }); 
-        }); 
-        
-        query.on('error', function(error) { 
-            console.log("ERROR!!" + error); 
-            response.render('index', {
-                title: title, data: null, message: "ERROR is occured!" 
-            }); 
-        });
-        
+    sendquery.on('error', err => { 
+        console.error(err.stack) 
+    });
+
+}
+
+//Search detail
+function contentlist_detail(req,res,next){ 
+    
+    var param_contentid = req.query.id || req.params.id;
+    
+    var pg_query = 
+        ` SELECT * FROM CONTENT CT 
+        INNER JOIN USERS U on U.user_id = CT.user_id 
+        WHERE CT.content_id = $1`;
+    
+    console.log("Query ==>",pg_query);
+    console.log("param_contentid ==>",param_contentid);
+
+    const sendquery = new Query(pg_query,[param_contentid]);
+    pool.query(sendquery);
+    var rows = []; 
+
+    sendquery.on("row",row=>{ rows.push(row); });
+    
+    sendquery.on('end', () => { 
+        console.log(rows); 
+        console.log('query done'); 
+        res.send(rows); res.status(200).end(); 
+    }); 
+
+    sendquery.on('error', err => { 
+        console.error(err.stack) 
     });
 }
+
 
 function addContent(req, res , next ){
-    var param_title = req.body.title || req.param.title;
-    var param_context = req.body.context || req.param.context;
-    var param_userid = req.body.user_id || req.param.user_id;
+    var param_userid = req.query.user_id || req.param.user_id;
+    var param_title = req.query.title || req.param.title;
+    var param_context = req.query.context || req.param.context;
     
-    pg.connect(con, function(err, client) { 
+    
 
-        var pg_query = 
-        ` INSERT INTO CONTENT
-        (user_id,title,context,created_at,update_at)
-        values ($1, $2, $3,now(),null)`;
+    var pg_query = 
+    ` INSERT INTO CONTENT
+    (user_id,title,context,created_at,update_at)
+    values ($1, $2, $3,now(),null)`;
 
-        var query = client.query(pg_query,[param_title, param_context, param_userid]); 
+    console.log("Query ==>",pg_query);
+    console.log("param_userid ==>",param_userid);
+    console.log("param_title ==>",param_title);
+    console.log("param_context ==>",param_context);
 
-        query.on('end', function(row,err) { response.redirect("/"); }); 
+    const sendquery = new Query(pg_query,[param_userid,param_title,param_context]);
+    pool.query(sendquery);
 
-        query.on('error', function(error) { 
-            console.log("ERROR!"); response.render('index', {
-                 title: "ERROR", data: null, message: "ERROR is occured!" 
-            });
+    var rows = []; 
+    //sendquery.on("row",row=>{ rows.push(row); });
+
+    sendquery.on('end', function(row,err) { 
+        //response.redirect("/"); 
+        console.log(rows); 
+        console.log('query done'); 
+        res.send(rows); res.status(200).end(); 
+    }); 
+
+    sendquery.on('error', function(error) { 
+        console.log("ERROR!"); response.render('index', {
+                title: "ERROR", data: null, message: "ERROR is occured!" 
         });
     });
 }
 
+
 function updateContent(req, res , next ){
-    var param_title = req.body.title || req.param.title;
-    var param_context = req.body.context || req.param.context;
-    var param_contentid = req.body.content_id || req.param.content_id;
-    
-    pg.connect(con, function(err, client) { 
+    var param_title = req.query.title || req.param.title;
+    var param_context = req.query.context || req.param.context;
+    var param_contentid = req.query.content_id || req.param.content_id;
 
-       //업데이트
-       var pg_query = " UPDATE CONTENT SET title = $1, context = $2 WHERE content_id = $3;";
+    //업데이트
+    var pg_query = " UPDATE CONTENT SET title = $1, context = $2 WHERE content_id = $3;";
 
-        var query = client.query(pg_query,[param_title, param_context, param_contentid]); 
+    console.log("Query ==>",pg_query);
+    console.log("param_title ==>",param_title);
+    console.log("param_context ==>",param_context);
+    console.log("param_contentid ==>",param_contentid);
 
-        query.on('end', function(row,err) { response.redirect("/"); }); 
+    const sendquery = new Query(pg_query,[param_title,param_context,param_contentid]);
+    pool.query(sendquery);
 
-        query.on('error', function(error) { 
-            console.log("ERROR!"); response.render('index', {
-                 title: "ERROR", data: null, message: "ERROR is occured!" 
-            });
+    var rows = []; 
+
+    sendquery.on('end', function(row,err) { 
+        console.log(rows); 
+        console.log('query done'); 
+        res.send(rows); res.status(200).end(); 
+    }); 
+
+    sendquery.on('error', function(error) { 
+        console.log("ERROR!"); response.render('index', {
+                title: "ERROR", data: null, message: "ERROR is occured!" 
         });
     });
 }
 
 function deleteContent(req , res , next){
-    var content_id = req.body.content_id || req.param.content_id;
+    var param_contentid = req.query.content_id || req.param.content_id;
     
-    pg.connect(con, function(err, client) { 
 
-        //업데이트
-        const pg_query = `delete from content where content_id = $1`;
+    //업데이트
+    const pg_query = `delete from content where content_id = $1`;
 
-        var query = client.query(pg_query,[content_id]); 
+    const sendquery = new Query(pg_query,[param_contentid]);
+    pool.query(sendquery);
 
-        query.on('end', function(row,err) { response.redirect("/"); }); 
+    var rows = []; 
 
-        query.on('error', function(error) { 
-            console.log("ERROR!"); response.render('index', {
-                 title: "ERROR", data: null, message: "ERROR is occured!" 
-            });
+    sendquery.on('end', function(row,err) { 
+        console.log(rows); 
+        console.log('query done'); 
+        res.send(rows); res.status(200).end(); 
+    }); 
+
+    sendquery.on('error', function(error) { 
+        console.log("ERROR!"); response.render('index', {
+                title: "ERROR", data: null, message: "ERROR is occured!" 
         });
     });
 }
-*/
+
 
 module.exports = router;
